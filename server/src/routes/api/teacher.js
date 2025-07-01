41a411fce26f78f7cb1fb2f5c059b26705bc84ab
Arif Shahriyar Nader <arif.nader95@gmail.com>
2025-07-01 11:51:40 +0600

    Add get a teacher by id logics

--- server/src/routes/api/teacher.js
+++ server/src/routes/api/teacher.js
@@ -24,4 +24,21 @@ router.post("/create-teacher", authenticateToken, async (req, res) => {
   }
 });
 
+router.get("/:id", authenticateToken, async (req, res) => {
+  const { id } = req.params;
+  try {
+    const result = await pool.query("SELECT * FROM teachers WHERE id = $1", [
+      id,
+    ]);
+    const teacher = result.rows[0];
+    if (!teacher) {
+      return res.status(404).json({ message: "Teacher not found" });
+    }
+    return res.status(200).json(teacher);
+  } catch (error) {
+    console.error("Error in fetching teacher:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
 export default router;
